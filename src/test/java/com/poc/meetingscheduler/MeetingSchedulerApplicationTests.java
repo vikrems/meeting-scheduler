dbdea6d855d49ed47cc22b0824eee4757e37ed21
vikshe <vikrem.sheker@hidglobal.com>
2023-03-03 12:08:00 +0530

    first commit

---
+++ src/test/java/com/poc/meetingscheduler/MeetingSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.meetingscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
